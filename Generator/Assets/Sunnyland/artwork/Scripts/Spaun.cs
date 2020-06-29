f2697a278884f94e8d4ab5855da9140b1bb458e2
Дмитрий Дудин <empire_95@mail.ru>
2020-06-30 00:35:40 +0300

    +

---
+++ Generator/Assets/Sunnyland/artwork/Scripts/Spaun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaun : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _spawnpoints;
+    [SerializeField] private GameObject _enemyPrefab;
+
+    private float _cooldown = 2f;
+    private float _currentTime;
+    private int _index = 0;
+
+    private void Start()
+    {
+        Instantiate(_enemyPrefab, _spawnpoints[_index].transform.position, Quaternion.identity);
+        _index++;
+    }
+
+    void Update()
+    {
+        _currentTime += Time.deltaTime;
+        if(_currentTime >= _cooldown)
+        {
+            Instantiate(_enemyPrefab, _spawnpoints[_index].transform.position, Quaternion.identity);
+            _index++;
+            _currentTime = 0;
+        }
+        if(_index == _spawnpoints.Length)
+        {
+            _index = 0;
+        }
+    }
+}
